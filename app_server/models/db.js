f491c60e6e606c179a144348f6572092a9fbb3c5
Ryan Smith <rtsmith27@gmail.com>
2020-07-17 19:49:58 -0500

    update db files

---
+++ app_server/models/db.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || 'mongodb://127.0.0.1/';
+const dbURL = `${host}`;
+const readLine = require ('readline');
+
+const connect = () => {
+    setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true }), 1000);
+  }
+
+const { hostname } = require('os');
+// const { connect } = require('http2');
+
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected');
+});
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error: ' + err);
+    return connect();
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose is disconnected');
+});
+
+
+if (process.platform === 'win32'){
+    const rl = readLine.createInterface ({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on ('SIGINT', () => {
+        process.emit ("SIGINT");
+    });
+}
+
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close( () => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    });
+};
+
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit (0);
+    });
+});
+connect();
+require('./locations');
