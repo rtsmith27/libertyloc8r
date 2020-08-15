21564e6639eb47600561c9160b3f4276ba525c5a
Ryan Smith <rtsmith27@gmail.com>
2020-08-15 16:09:17 -0500

    Update try for ch 8 & 9

--- app_public/src/app/loc8r-data.service.ts
+++ app_public/src/app/loc8r-data.service.ts
@@ -9,10 +9,13 @@ export class Loc8rDataService {
 
   constructor(private http: HttpClient) { }
   
-    // private apiBaseUrl = 'http://localhost:3000/api'; //use this for devlopment
-    private apiBaseUrl = 'https://libertyloc8r.herokuapp.com/api'; /*input heroku url for publishing */
+    // private apiBaseUrl = 'http://localhost:3000/api' //use this for devlopment
+    private apiBaseUrl = 'https://libertyloc8r.herokuapp.com/api'/*input heroku url for publishing */
     
     public getLocations(lat: number, lng: number): Promise<Location[]> {
+    // public getLocztions(): Promise<Location[]> {
+    //   const lng: number = -94.410569;
+    //   const lat: number =  39.234315;
       const maxDistance: number = 20000;
       const url: string = `${this.apiBaseUrl}/locations?lng=${lng}&lat=${lat}&maxDistance=${maxDistance}`;
       return this.http
@@ -27,3 +30,4 @@ export class Loc8rDataService {
       return Promise.reject(error.message || error);
     }
   }
+ 
